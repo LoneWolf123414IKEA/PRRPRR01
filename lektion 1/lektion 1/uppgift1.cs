a019b3ad797129b4169f05bb49316b704a0bb884
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-08-31 12:06:33 +0200

    first lesson

---
+++ lektion 1/lektion 1/uppgift1.cs
@@ -0,0 +1,17 @@
+﻿/*using System;
+
+namespace uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;//sets the colour of the background
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;//sets text-colour
+            Console.WriteLine("Eloo World");//prints a message
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();//holds window open till someone presses a key
+        }
+    }
+}*/
---
+++ uppgift2/uppgift2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1.");
+            Console.WriteLine("\r\nDu har lärt dig skriva ut tecken som \" och \\.");
+        }
+    }
+}
