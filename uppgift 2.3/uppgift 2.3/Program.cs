bb9af3ecc6cb0f6be95e89ea7aa6407339eac06e
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-09-07 12:12:22 +0200

    uppgift 2.3

---
+++ uppgift 2.3/uppgift 2.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace kap2upp3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How high can you jump in high jump? please awnser in meters.");
+            string jumptxt = Console.ReadLine();
+            float jump = float.Parse(jumptxt);
+            Console.WriteLine("You can jump " + (2.45-jump) + " meters lower than the world record!");
+    }
+        }
+}
