4774e99d072eb8e0de34daaa2c0a503061c613c2
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-09-07 11:50:54 +0200

    uppgift 2.1

---
+++ uppgift 2.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace kap2upp1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What city are you from?");
+            string hometw = Console.ReadLine();
+            Console.WriteLine("Where in the world would you most want to visit?");
+            string destin = Console.ReadLine();
+            Console.WriteLine("Hello " + hometw + "-inhabitant, I hope you get to go to " + destin);
+        }
+    }
+}
