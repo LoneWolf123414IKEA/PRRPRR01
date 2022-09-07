3bee315653902f9736909366dfd18f923bc8cd72
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-09-07 12:03:18 +0200

    uppgift 2.2

---
+++ uppgift 2.2/uppgift 2.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace kap2upp2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What year is it?!");
+            string yeartxt = Console.ReadLine();
+            int year = int.Parse(yeartxt);
+            Console.WriteLine("the year in 20 years will be " + (year+20));
+        }
+    }
+}
