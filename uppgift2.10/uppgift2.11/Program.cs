dc83644fc2684f68c247af83c95c16b4e0f711ed
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-09-14 11:37:03 +0200

    Add files via upload

---
+++ uppgift2.10/uppgift2.11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace kap2upp11
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how long did Alma jump in metres?");
+            float Almanum = float.Parse(Console.ReadLine());
+            Console.WriteLine("how long did Elin jump in metres?");
+            float Elinnum = float.Parse(Console.ReadLine());
+            Console.WriteLine("Elin jumped " + (Elinnum - Almanum) + "m more than Alma.");
+        }
+    }
+}
