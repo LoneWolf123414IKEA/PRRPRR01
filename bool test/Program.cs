5c7e41e72242e0b2f72b75d45846d420106b0ae3
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-09-23 11:13:57 +0200

    Add files via upload

--- bool test/Program.cs
+++ bool test/Program.cs
@@ -1,82 +1,77 @@
 ﻿using System;
 namespace Booltest
 {
-    class program
+    class Program
     {
         static void Main(string[] args)
         {
-            string Crrawn;
             int pnt = 0;
-            Console.WriteLine("welcome to this quiz");
+            Console.WriteLine("welcome to this quiz\nThe consept is quite simple, type the letter of the correct awnser and gain a point.\nDepending on how you do the difficulty will be adjusted during the course of the quiz!");
 
-            Crrawn = "B";
-            Console.WriteLine("What is the capital of India?\nA:Bogota\nB:New Delhi\nC:Calcuta");
-            if (Console.ReadLine().ToUpper() == Crrawn)
+            Console.WriteLine("\nWhat is the capital of India?\nA:Bogota\nB:New Delhi\nC:Calcuta");
+            if (Console.ReadLine()?.ToUpper() == "B")
             {
                 pnt += 1;
             }
 
-            Crrawn = "A";
             switch (pnt)
             {
                 case 0:
-                    Console.WriteLine("What sort of creature is a cassowary?\nA:Bird\nB:Fish\nC:Crab");
+                    Console.WriteLine("\nWhat sort of creature is a cassowary?\nA:Bird\nB:Fish\nC:Crab");
                     break;
                 case 1:
-                    Console.WriteLine("How many surfaces does a Möbius strip have?\nA:1\nB:2\nC:3");
+                    Console.WriteLine("\nHow many surfaces does a Möbius strip have?\nA:1\nB:2\nC:3");
                     break;
                 default:
                     break;
             }
-            if (Console.ReadLine().ToUpper() == Crrawn)
+            if (Console.ReadLine()?.ToUpper() == "A")
             {
                 pnt += 1;
             }
 
-            Crrawn = "C";
             switch (pnt)
             {
                 case 0:
-                    Console.WriteLine("question?\nA:awnser\nB:awnser\nC:awnser");
+                    Console.WriteLine("\nWhere did the Vietnam war take place?\nA:Hong-Kong\nB:Viet-Naam\nC:None of the above");
                     break;
                 case 1:
-                    Console.WriteLine("question?\nA:awnser\nB:awnser\nC:awnser");
+                    Console.WriteLine("\nWhere did the Vietnam war take place?\nA:North Borneo\nB:The East Indies\nC:French Indo-China");
                     break;
                 case 2:
-                    Console.WriteLine("question?\nA:awnser\nB:awnser\nC:awnser");
+                    Console.WriteLine("\nquestion?\nA:awnser\nB:awnser\nC:awnser");
                     break;
                 default:
                     break;
             }
-            if (Console.ReadLine().ToUpper() == Crrawn)
+            if (Console.ReadLine()?.ToUpper() == "C")
             {
                 pnt += 1;
             }
 
-            Crrawn = "A";
             switch (pnt)
             {
                 case 0:
-                    Console.WriteLine("question?\nA:awnser\nB:awnser\nC:awnser");
+                    Console.WriteLine("\nquestion?\nA:awnser\nB:awnser\nC:awnser");
                     break;
                 case 1:
-                    Console.WriteLine("question?\nA:awnser\nB:awnser\nC:awnser");
+                    Console.WriteLine("\nWho wrote the hit album \"Born in the U.S.A.\"?\nA:Bruce Springsteen\nB:Black sabbath\nC:Green Day");
                     break;
                 case 2:
-                    Console.WriteLine("question?\nA:awnser\nB:awnser\nC:awnser");
+                    Console.WriteLine("\nquestion?\nA:awnser\nB:awnser\nC:awnser");
                     break;
                 case 3:
-                    Console.WriteLine("question?\nA:awnser\nB:awnser\nC:awnser");
+                    Console.WriteLine("\nquestion?\nA:awnser\nB:awnser\nC:awnser");
                     break;
                 default:
                     break;
             }
-            if (Console.ReadLine().ToUpper() == Crrawn)
+            if (Console.ReadLine()?.ToUpper() == "A")
             {
                 pnt += 1;
             }
 
-            Console.WriteLine(pnt);
+            Console.WriteLine("\nyou have gatherd together " + pnt + " points!"); 
         }
     }
 }
