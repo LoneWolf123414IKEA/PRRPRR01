a8a87f0bc61e3c0e1108fa8e51e965cadf6a31da
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-09-30 10:47:45 +0200

    Add files via upload

---
+++ Upp3.15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace kap3upp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How long do yo want to rent a car? (Please enter in full hours)");
+            int renttime = int.Parse(Console.ReadLine());
+            renttime *= 80;
+            if (renttime < 950)
+            {
+                Console.WriteLine("you will pay " + renttime + " kr");
+            }
+            else
+            {
+                Console.WriteLine("Sorry but you have exeeded the max rent");
+            }
+        }
+    }
+}
