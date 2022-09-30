3b377d808db9d37ab9a8aa6652190948b520af92
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-09-30 10:37:55 +0200

    Add files via upload

---
+++ Upp3.14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace kap3upp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Have you graduated from highschool?(Y/N)");
+            bool yn = Console.ReadLine() == "Y";
+            Console.WriteLine("How old are you?");
+            if (yn && int.Parse(Console.ReadLine()) >= 22)
+            {
+                Console.WriteLine("We would like to hire you");
+            }
+            else 
+            {
+                Console.WriteLine("Sorry but you do not fit our current requirements"); 
+            }
+        }
+    }
+}
