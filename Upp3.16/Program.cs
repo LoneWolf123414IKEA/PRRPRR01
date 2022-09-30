47614dac39f3baae932a4206c52b23a18de977b6
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-09-30 11:07:06 +0200

    Add files via upload

---
+++ Upp3.16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace kap3upp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the playtime(i.e how long it is) of your song, first minutes then seconds");
+            int playtime = (int.Parse(Console.ReadLine()) * 60);
+            playtime += int.Parse(Console.ReadLine());
+            if (playtime >= 165 && playtime <= 260)
+            {
+                Console.WriteLine("We can play your song on air");
+            }
+            else
+            {
+                Console.WriteLine("Sorry but your song is not accaptable");
+            }
+        }
+    }
+}
