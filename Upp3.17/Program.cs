dd27aa50af4d56e6ae384292a449357e1abb99bf
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-09-30 11:27:35 +0200

    Add files via upload

---
+++ Upp3.17/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace kap3upp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first number");
+            int num = (int.Parse(Console.ReadLine()));
+            Console.WriteLine("please enter the second number");
+            int num2 = (int.Parse(Console.ReadLine()));
+            Console.WriteLine("please choose which opperator you wish to use:\n1:Addition\n2:Subtraction\n3:Multiplication\n4:Division");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine(num + num2);
+                    break;
+                case "2":
+                    Console.WriteLine(num - num2);
+                    break;
+                case "3":
+                    Console.WriteLine(num * num2);
+                    break;
+                case "4":
+                    Console.WriteLine(num / num2);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
