275f1aec1a0e44a2b97123797b21b55555338e55
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-09-30 10:29:31 +0200

    Add files via upload

---
+++ upp3.13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace kap3upp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how old are you?");
+            int age = int.Parse(Console.ReadLine());
+            if (age < 16)
+            {
+                Console.WriteLine("you are too young to enter the competition");
+            }
+            else if (age > 19)
+            {
+                Console.WriteLine("you are too old to enter the competition");
+            }
+            else
+            {
+                Console.WriteLine("you may enter this competiton");
+            }
+        }
+    }
+}
