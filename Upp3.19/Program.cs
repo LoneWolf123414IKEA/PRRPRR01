7ee61c59c087fad10b33e2dbf1e07df0ba7e0b8f
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-10-05 11:58:30 +0200

    Add files via upload

---
+++ Upp3.19/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+namespace Kap3Upp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your calculation");
+            var result = new DataTable().Compute(Console.ReadLine(), null);
+            Console.WriteLine(result);
+            for()
+        }
+    }
+}
