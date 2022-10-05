4ae44a943a16ddf23d1dfddf1ec072fe25005f17
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-10-05 12:07:26 +0200

    Add files via upload

---
+++ Upp3.20/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Kap3Upp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your three words");
+            string[] words = { Console.ReadLine(), Console.ReadLine(), Console.ReadLine() };
+            Array.Sort(words);
+            Console.WriteLine(words[0] + " " + words[1] + " " + words[2]);
+        }
+    }
+}
