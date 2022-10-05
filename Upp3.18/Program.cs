d28f988ba61a7bd7c270bdee9bcfa9128ce0a6a8
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-10-05 11:47:23 +0200

    Add files via upload

---
+++ Upp3.18/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace kap3upp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please write your first and last name");
+            string[] nameli = Console.ReadLine().Split(" ");
+            Array.Sort(nameli);
+            Console.WriteLine(nameli[0] + " comes before " + nameli[1] + " alphabetically");
+        }
+    }
+}
