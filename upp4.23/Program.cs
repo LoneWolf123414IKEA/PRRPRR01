aaaa07952206df0963977e92761b04ed9c67d434
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-10-19 11:57:17 +0200

    Add files via upload

---
+++ upp4.23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Upp23kap4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string contin;
+            List<int> nums = new List<int>();
+            while(true)
+            {
+                Console.WriteLine("Write a number or write \"n\" if you want to print the biggest one");
+                contin = Console.ReadLine();
+                if (contin == "n")
+                {
+                    break;
+                }
+                else
+                {
+                    nums.Add(int.Parse(contin));
+                }
+            }
+            nums.Sort();
+            nums.Reverse();
+            int[] numsa = nums.ToArray();
+            Console.WriteLine(numsa[0]);
+        }
+    }
+}
