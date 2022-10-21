9d74b25a9b872a612304b3f78786bb15237095fd
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-10-21 11:05:35 +0200

    Add files via upload

---
+++ upp4.28/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace kap4upp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "ab";
+            string expl = Console.ReadLine();
+            bool a = false;
+            bool b = false;
+            foreach(char i in test)
+            {
+                foreach (char c in expl)
+                {
+                    if (i == c && i == 'a')
+                    {
+                        a = true;
+                        break;
+                    }
+                    else if (i == c)
+                    {
+                        b = true;
+                        break;
+                    }
+                }
+                if (a && b)
+                {
+                    Console.WriteLine("both a and b are inn the text");
+                }
+            }
+        }
+    }
+}
