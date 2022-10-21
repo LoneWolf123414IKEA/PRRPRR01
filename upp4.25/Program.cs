7c32488b715489b0b5013f3d0aea1465647118cd
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-10-21 10:15:24 +0200

    Add files via upload

---
+++ upp4.25/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Upp24Kap4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            bool dubblbreak = true;
+            while (dubblbreak)
+            {
+                Console.WriteLine("Choose betweeen theese options:\n1. Subtract a number from another\n2. Divide a nnumber by another\n3. Exit the program"); 
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine(int.Parse(Console.ReadLine()) - int.Parse(Console.ReadLine()));
+                        break;
+                    case "2":
+                        Console.WriteLine(int.Parse(Console.ReadLine()) / int.Parse(Console.ReadLine()));
+                        break;
+                    default:
+                        dubblbreak = false;
+                        break;
+                }
+            }
+        }
+    }
+}
