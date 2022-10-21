a386f86aff1958b723a4efd03c4e7c0704aad1f5
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-10-21 10:52:40 +0200

    Add files via upload

---
+++ upp4.27/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace kap4upp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "ayup";
+            string lttr;
+            int steps = int.Parse(Console.ReadLine());
+            foreach (char i in text)
+            {
+                lttr = i.ToString();
+                for (int j = 0; j < steps; j++)
+                {
+                    lttr = " " + lttr;
+                }
+                Console.WriteLine(lttr);
+            }
+        }
+    }
+}
