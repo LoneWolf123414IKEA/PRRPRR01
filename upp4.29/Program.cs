cbde265b0cb218d6e9a11437b58261c2b2109a28
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-10-28 10:19:36 +0200

    Add files via upload

---
+++ upp4.29/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace upp29kap4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("how long do you want your triangl: ");
+            int length = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
