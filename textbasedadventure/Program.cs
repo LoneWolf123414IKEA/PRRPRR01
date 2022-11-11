fa3765ec6c76f621775b4ec5d6da5e9d170caaed
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-11-11 11:12:19 +0100

    Update Program.cs

--- textbasedadventure/Program.cs
+++ textbasedadventure/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 namespace tba
 {
     class program
@@ -8,43 +8,70 @@ namespace tba
             string[] enemy = { "skeleton", "knight" };
             Random rand = new Random();
             int hp = 125;
-            int res = 10;
+            float res = 0.1;
             int dmg = 5;
             int ehp;
-            int eres;
+            float eres;
             int edmg;
+            int tempdmg;
+            int etempdmg;
+            string opt;
             while (hp > 0)
             {
                 switch (enemy[rand.Next(enemy.Length)])
                 {
                     case "skeleton":
                         ehp = rand.Next(10, 21);
-                        eres = rand.Next(5, 14);
+                        eres = rand.Next(5, 14)´/100;
                         edmg = rand.Next(5, 11);
-                        while (ehp > 0)
+                        while (ehp > 0 && hp > 0)
                         {
                             Console.Clear();
-                            Console.WriteLine($"Skeleton:\nHp: {ehp}\nRes: {eres}\nDmg: {edmg}-{edmg + 5}");
-                            Console.WriteLine("1. attack\n2. defend\n3. run");
-                            switch (Console.Read())
+                            Console.WriteLine($"Skeleton:\nHp: {ehp}\nRes: {eres}\nDmg: {edmg}-{edmg + 5}\n\nPlayer:\nHp: {hp}\nRes: {res}\nDmg: {dmg}-{dmg + 5}\n");
+                            Console.WriteLine("1. attack 2. item 3. run");
+                            opt = Console.ReadLine();
+                            tempdmg = 0;
+                            Console.Clear();
+                            switch (opt)
                             {
-                                case 1:
-                                    ehp -= rand.Next(dmg, dmg+5);
-                                    break;                                                                                                               
-                                case 2:
+                                case "1":
+                                    if (rand.Next(100) < 88)
+                                    {
+                                        tempdmg = rand.Next(dmg, dmg + 5);
+                                    }
+                                    else
+                                    {
+                                        tempdmg = rand.Next(dmg, dmg + 5) - 10;
+                                        Console.WriteLine("Enemy resisted");
+                                    }
                                     break;
-                                case 3:
+                                case "2":
+                                    break;
+                                case "3":
                                     break;
                                 default:
                                     break;
                             }
+                            if (rand.Next(100) < 88)
+                            {
+                                etempdmg = rand.Next(edmg, edmg + 5);
+                            }
+                            else
+                            {
+                                etempdmg = rand.Next(edmg, edmg + 5) - 10;
+                                Console.WriteLine("You resisted");
+                            }
+                            Console.WriteLine($"You did {tempdmg} damage\nYou lost {etempdmg} health");
+                            hp -= etempdmg;
+                            ehp -= tempdmg;
+                            Console.ReadKey();
                         }
                         break;
                     case "knight":
-                        break ;
+                        break;
                     default:
                         break;
-
+     
                 }
             }
         }
