c217a2aec6fee616db72a0cef621c01f3022d034
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2022-11-18 11:14:54 +0100

    Update Program.cs

--- textbasedadventure/Program.cs
+++ textbasedadventure/Program.cs
@@ -5,15 +5,35 @@ namespace tba
     {
         static void Main(string[] args)
         {
-            string[] enemy = { "skeleton", "knight" };
-            string[] weapons = { "wooden sword", "club"};
-            float[] weaponsdmg = {1, 2 };
-            string[] items = { "small heal", "smaller heal"};
-            float[] itemssstt = { 1, 0.1F };
+            string[] enemy = 
+            { 
+                "skeleton", 
+                "knight" 
+            };
+            string[] weapons = 
+            { 
+                "wooden sword",
+                "club" 
+            };
+            int[] weaponsdmg = 
+            { 
+                10, 
+                11 
+            };
+            string[] items = 
+            { 
+                "small heal", 
+                "smaller heal" 
+            };
+            float[] itemssstt = 
+            { 
+                1, 
+                0.1F 
+            };
             List<int> wepinv = new List<int>();
             List<int> inv = new List<int>();
-            int maxwep = 3;
-            int maxinv = 6;
+            int maxwep = 1;
+            int maxinv = 2;
             Random rand = new Random();
             float hp = 125;
             float res = 0.1F;
@@ -24,8 +44,68 @@ namespace tba
             float tempdmg;
             float etempdmg;
             string opt;
+            int temp;
             while (hp > 0)
             {
+                Console.Clear();
+                Console.WriteLine("do you want too \"loot\" or \"kick door\"");
+                if (Console.ReadLine() == "loot")
+                {
+                    temp = rand.Next(99);
+                    if (temp < 49 && inv.Count() <= maxinv)
+                    {
+                        temp = rand.Next(items.Length);
+                        Console.WriteLine("do you want " + items[temp] + "?");
+                        if (Console.ReadLine() == "yes")
+                        {
+                            inv.Add(temp);
+                        }
+                    }
+                    else if (temp < 49 && inv.Count() > maxinv)
+                    {
+                        temp = rand.Next(items.Length);
+                        Console.WriteLine("do you want " + items[temp] + "?");
+                        if (Console.ReadLine() == "yes")
+                        {
+                            for (int i = 0; i < inv.Count(); i++)
+                            {
+                                Console.WriteLine((i+1) + " " + items[inv[i]]);
+                            }
+                            Console.WriteLine("\nchoose item to discard");
+                            inv.RemoveAt(int.Parse(Console.ReadLine()) - 1);
+                            inv.Add(temp);
+                        }
+                    }
+                    else if (temp > 12 && wepinv.Count() <= maxwep)
+                    {
+                        temp = rand.Next(weapons.Length);
+                        Console.WriteLine("do you want " + weapons[temp] + "?");
+                        if (Console.ReadLine() == "yes")
+                        {
+                            wepinv.Add(temp);
+                        }
+                    }
+                    else if (temp > 12 && wepinv.Count() > maxwep)
+                    {
+                        temp = rand.Next(weapons.Length);
+                        Console.WriteLine("do you want " + weapons[temp] + "?");
+                        if (Console.ReadLine() == "yes")
+                        {
+                            for (int i = 0; i < wepinv.Count(); i++)
+                            {
+                                Console.WriteLine((i + 1) + " " + weapons[wepinv[i]]);
+                            }
+                            Console.WriteLine("\nchoose item to discard");
+                            wepinv.RemoveAt(int.Parse(Console.ReadLine()) - 1);
+                            wepinv.Add(temp);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("you found nothing)");
+                        Console.ReadKey();
+                    }
+                }
                 switch (enemy[rand.Next(enemy.Length)])
                 {
                     case "skeleton":
@@ -43,6 +123,7 @@ namespace tba
                             switch (opt)
                             {
                                 case "1":
+                                    Console.WriteLine();
                                     if (rand.Next(100) < 88)
                                     {
                                         tempdmg = rand.Next(dmg, dmg + 5);
@@ -54,6 +135,17 @@ namespace tba
                                     }
                                     break;
                                 case "2":
+                                    Console.WriteLine("1. attack\n2. heal");
+                                    if (Console.ReadLine() == "1")
+                                    {
+                                        Console.WriteLine("choose weapon");
+                                        for (int i = 0; i < wepinv.Count(); i++)
+                                        {
+                                            Console.WriteLine((i + 1) + " " + weapons[wepinv[i]]);
+                                        }
+                                        temp = int.Parse(Console.ReadLine()) - 1;
+                                        tempdmg = rand.Next(weaponsdmg[temp], weaponsdmg[temp] + 5);
+                                    }
                                     break;
                                 case "3":
                                     break;
