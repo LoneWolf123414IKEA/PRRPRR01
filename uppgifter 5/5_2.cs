0657f1b5f69d7f8b4a1b6ca11b938f25be1c0c68
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2023-01-20 10:18:06 +0100

    Update 5_2.cs

--- uppgifter 5/5_2.cs
+++ uppgifter 5/5_2.cs
@@ -1,1 +1,15 @@
-
+using System;
+namespace kap5upp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please first input the amount of numbers you want to enter, then input that amount of whole numbers:");
+            string[] sentinces = new string[int.Parse(Console.ReadLine())];
+            for (int i = 0; i < sentinces.Length; i++) sentinces[i] = "#" + (i + 1) + ": " + Console.ReadLine();
+            Console.WriteLine("\nHere are your numbers numberd:");
+            foreach (string i in sentinces) Console.WriteLine(i);
+        }
+    }
+}
