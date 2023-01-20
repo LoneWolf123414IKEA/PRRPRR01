679f3a5e82f8e8cf51005eff6e429c7ab0ed3282
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2023-01-20 10:06:29 +0100

    Update 5.1.cs

--- uppgifter 5/5.1.cs
+++ uppgifter 5/5.1.cs
@@ -1,1 +1,12 @@
-
+using System;
+namespace kap5upp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sentinces = { "Eloo, World!", "Thats an amazing hat!", "This is a third sentince." };
+            foreach (string i in sentinces) Console.WriteLine(i + "\n");
+        }
+    }
+}
