0d07ca5b80c1cc820b243276c6f5c0120a45b8e0
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2023-01-25 12:10:45 +0100

    Update 5_10.cs

--- uppgifter 5/5_10.cs
+++ uppgifter 5/5_10.cs
@@ -1,1 +1,20 @@
+using System;
 
+namespace kap5upp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] h = new int[rand.Next(100000)];
+            Console.WriteLine(h.Length);
+            for (int i = 0; i < h.Length; i++) h[i]=rand.Next(100);
+            foreach (int i in h.Distinct().ToArray())
+            {
+                h.Count(i);
+            }
+           
+        }
+    }
+}
