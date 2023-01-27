0def9447834f11a46c0acc9677f0c162026963f3
LoneWolf123414IKEA <111962453+LoneWolf123414IKEA@users.noreply.github.com>
2023-01-27 10:22:52 +0100

    Update 5_10.cs

--- uppgifter 5/5_10.cs
+++ uppgifter 5/5_10.cs
@@ -9,12 +9,29 @@ namespace kap5upp8
             Random rand = new Random();
             int[] h = new int[rand.Next(100000)];
             Console.WriteLine(h.Length);
-            for (int i = 0; i < h.Length; i++) h[i]=rand.Next(100);
+            for (int i = 0; i < h.Length; i++) h[i] = rand.Next(100);
+            Array.Sort(h);
+            int temp = 0;
+            int stor = 0;
+            List<int> scor = new List<int>();
             foreach (int i in h.Distinct().ToArray())
             {
-                h.Count(i);
+                foreach (int j in h)
+                {
+                    if (j == i) temp++;
+                }
+                if (temp == stor) scor.Add(i);
+                else if (temp > stor)
+                {
+                    stor = temp;
+                    scor.Clear();
+                    scor.Add(i);
+                }
+                temp = 0;
             }
-           
+            scor.ToArray();
+            Console.Write("the most common numbers are: ");
+            foreach (int i in scor) Console.Write(i + " ");
         }
     }
 }
